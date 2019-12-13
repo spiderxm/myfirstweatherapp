8e1484b9ea2567e771e72f3d7a8fcbe24e1ae93e
Mrigank Anand <mrigank.rkee@rediffmail.com>
2019-12-13 18:52:46 +0530

    field to store weather data string containing dummy data appending each string from the fake weather string

--- app/src/main/java/com/example/myfirstweatherapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstweatherapp/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.myfirstweatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView mWheatherTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWheatherTextView=(TextView)findViewById(R.id.tv_weather_data);
+        String[] dummyWeatherData={
+                "Today, MAy 17 - clear - 17c/15.c",
+        };
+
+        for(String dummyWeatherDay:dummyWeatherData)
+        {
+            mWheatherTextView.append(dummyWeatherDay+"\n\n\n");
+        }
     }
 }
